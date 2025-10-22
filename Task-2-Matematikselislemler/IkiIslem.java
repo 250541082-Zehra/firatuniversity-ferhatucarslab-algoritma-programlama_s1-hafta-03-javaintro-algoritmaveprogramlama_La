3da108764c4a75a642d9c69fa11e72b72854b88f
250541082-Zehra <250541082@firat.edu.tr>
2025-10-22 13:50:58 +0300

    Update IkiIslem.java

---
+++ Task-2-Matematikselislemler/IkiIslem.java
@@ -0,0 +1,12 @@
+public class FaktoriyelHesapla {
+    public static void main(String[] args) {
+
+        int sayi = 5;
+        long faktoriyel = 1;
+        for(int i = 1; i <= sayi; i++) {
+            faktoriyel = faktoriyel * i;
+        }
+
+        System.out.println(sayi + "! = " + faktoriyel);
+    }
+}

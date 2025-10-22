4aae07489edeac7b4c252eac3a0013e40c23da08
250541082-Zehra <250541082@firat.edu.tr>
2025-10-22 12:38:51 +0300

    Add JavaDeseni class with welcome messages

---
+++ Task-1-HosgeldinMesajlari/JavaDeseni.java
@@ -0,0 +1,17 @@
+public class JavaDeseni {
+    public static void main(String[] args) {
+
+        // 1. Satır: J A V V A
+        System.out.println("J A V V A");
+
+        // 2. Satır: J A A V V A A
+        System.out.println("J A A V V A A");
+
+        // 3. Satır: J J A A A A V V A A A A
+        System.out.println("J J A A A A V V A A A A");
+
+        // 4. Satır: J J A A V A A
+        System.out.println("J J A A V A A");
+
+    }
+}

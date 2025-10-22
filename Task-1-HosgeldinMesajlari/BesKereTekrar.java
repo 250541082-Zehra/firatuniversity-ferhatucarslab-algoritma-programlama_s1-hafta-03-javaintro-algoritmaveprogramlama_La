b72c06d32919e0229735970411780c938d687b2a
250541082-Zehra <250541082@firat.edu.tr>
2025-10-22 11:50:37 +0300

    Create HosGeldin class to print welcome message

---
+++ Task-1-HosgeldinMesajlari/BesKereTekrar.java
@@ -0,0 +1,9 @@
+final class HosGeldin {
+    public static void main(String[] args) {
+        System.out.println("Java'ya hoş geldin");
+        System.out.println("Java'ya hoş geldin");
+        System.out.println("Java'ya hoş geldin");
+        System.out.println("Java'ya hoş geldin");
+        System.out.println("Java'ya hoş geldin");
+    }
+}

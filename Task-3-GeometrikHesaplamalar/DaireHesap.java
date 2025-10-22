f89924645f320bb8e57a0e0c4dac79a504801217
250541082-Zehra <250541082@firat.edu.tr>
2025-10-22 19:42:21 +0300

    Add DaireHesaplama class for circle calculations

---
+++ Task-3-GeometrikHesaplamalar/DaireHesap.java
@@ -0,0 +1,12 @@
+public class DaireHesaplama {
+    public static void main(String[] args) {
+        int yaricap = 2;
+        int pi = 3;
+        int alan = pi * yaricap * yaricap;
+        int cevre = 2 * pi * yaricap;
+        System.out.println("Yarıçap (r): " + yaricap);
+        System.out.println("Pi Sayısı (pi): " + pi);
+        System.out.println("Dairenin Alanı: " + alan);
+        System.out.println("Dairenin Çevresi: " + cevre);
+    }
+}

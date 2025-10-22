a3c8820457fd9324771550c5633704235fb3409f
250541082-Zehra <250541082@firat.edu.tr>
2025-10-22 20:14:16 +0300

    Replace AsciiArtKalp with AsciiArtZHarfi

--- Task-4-StringDesenler/AsciiArtHarf.java
+++ Task-4-StringDesenler/AsciiArtHarf.java
@@ -1,14 +1,16 @@
-public class AsciiArtKalp {
+public class AsciiArtZHarfi {
+
     public static void main(String[] args) {
 
-        System.out.println("  *** *** ");
-        System.out.println(" ************");
-        System.out.println(" ************");
-        System.out.println("  ********** ");
-        System.out.println("   ******** ");
-        System.out.println("    ****** ");
-        System.out.println("     **** ");
-        System.out.println("      ** ");
+        System.out.println("ZZZZZZZ");
+        System.out.println("      Z");
+        System.out.println("     Z ");
+        System.out.println("    Z  ");
+        System.out.println("   Z   ");
+        System.out.println("  Z    ");
+        System.out.println(" Z     ");
+        System.out.println("ZZZZZZZ");
 
     }
 }
+

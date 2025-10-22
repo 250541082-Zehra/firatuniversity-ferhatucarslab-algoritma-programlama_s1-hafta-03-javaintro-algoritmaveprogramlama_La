5826c7dda2a0fcc417437ffbdbbba18513838ea4
250541082-Zehra <250541082@firat.edu.tr>
2025-10-22 20:06:36 +0300

    Add ASCII art heart in AsciiArtHarf.java

---
+++ Task-4-StringDesenler/AsciiArtHarf.java
@@ -0,0 +1,14 @@
+public class AsciiArtKalp {
+    public static void main(String[] args) {
+
+        System.out.println("  *** *** ");
+        System.out.println(" ************");
+        System.out.println(" ************");
+        System.out.println("  ********** ");
+        System.out.println("   ******** ");
+        System.out.println("    ****** ");
+        System.out.println("     **** ");
+        System.out.println("      ** ");
+
+    }
+}

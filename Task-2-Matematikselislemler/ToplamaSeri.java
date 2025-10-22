8f7625b70753b8d1f6a4ed0581958bda0b9f1733
250541082-Zehra <250541082@firat.edu.tr>
2025-10-22 13:27:14 +0300

    Add ToplamaSeri.java for summing numbers 1 to 9

---
+++ Task-2-Matematikselislemler/ToplamaSeri.java
@@ -0,0 +1,13 @@
+public class ToplamaSeri {
+    public static void main(String[] args) {
+
+        // Sonucu hesapla
+        // Toplama işlemi doğrudan yapılıyor: 1+2+3+4+5+6+7+8+9 = 45
+        int sonuc = 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9;
+
+        // Ekrana yazdir
+        // "1+2+...+9 = " metni ile 'sonuc' değişkeninin değeri birleştirilip yazdırılıyor.
+        System.out.println("1+2+...+9 = " + sonuc);
+
+    }
+}

77c709bcad1c651bec97939287869bcf07f56c11
250541082-Zehra <250541082@firat.edu.tr>
2025-10-22 19:48:27 +0300

    Update UcgenAlanHesap.java

---
+++ Task-3-GeometrikHesaplamalar/UcgenAlanHesap.java
@@ -0,0 +1,12 @@
+public class UcgenAlanHesaplama {
+    public static void main(String[] args) {
+        int dikKenarA = 3;
+        int dikKenarB = 4;
+        int hipotenusC = 5;
+        int alan = (dikKenarA * dikKenarB) / 2;
+        System.out.println("Üçgen Kenar Uzunlukları: 3, 4, 5 (Özel Dik Üçgen)");
+        System.out.println("Dik Kenar A: " + dikKenarA);
+        System.out.println("Dik Kenar B: " + dikKenarB);
+        System.out.println("Üçgenin Alanı: " + alan);
+    }
+}

62fb971fb0a20e4d2e49dfb7613417521891ddbd
250541082-Zehra <250541082@firat.edu.tr>
2025-10-22 19:57:23 +0300

    Add rectangle area calculation program

---
+++ Task-3-GeometrikHesaplamalar/DikdortgenHesap.java
@@ -0,0 +1,10 @@
+public class DikdortgenAlanHesaplama {
+    public static void main(String[] args) {
+        int kisaKenar = 5;
+        int uzunKenar = 17;
+        int alan = kisaKenar * uzunKenar;
+        System.out.println("Kısa Kenar: " + kisaKenar + " cm");
+        System.out.println("Uzun Kenar: " + uzunKenar + " cm");
+        System.out.println("Dikdörtgenin Alanı: " + alan + " cm²");
+    }
+}
